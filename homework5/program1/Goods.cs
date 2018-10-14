fdffc51c6bca89944d71a177d8d6089981249040
克桥 韦 <1264251638@qq.com>
2018-10-14 23:34:50 +0800

    10/14

---
+++ homework5/program1/Goods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Goods
+    {
+        public Goods(string goods)
+        {
+            if (goods.Equals("辣条"))
+            {
+                this.Id = "2018101301";
+                this.Price = 0.5F;
+                this.Name = goods;
+            }
+            else if (goods.Equals("七个小矮人"))
+            {
+                this.Id = "2018101302";
+                this.Price = 1.0f;
+                this.Name = goods;
+            }
+            else if (goods.Equals("快乐水"))
+            {
+                this.Id = "2018101303";
+                this.Price = 3.0f;
+                this.Name = goods;
+            }
+            else
+            {
+                throw new Exception("没有此商品");
+            }            
+        }
+
+        public string Id
+        {
+            get;
+
+        }
+        public float Price
+        {
+            get;
+        }
+        public string Name
+        {
+            get;
+        }
+    }
+}
